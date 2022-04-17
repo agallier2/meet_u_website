08f468056be1164b8483d06ce24ac7c3f1a9eb60
govgovnahr <govindn3@illinois.edu>
2022-04-16 22:07:47 -0500

    made react app

---
+++ meetu/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          audrey has impostor syndrome :(
+        </p>
+        <a>
+          Hi Lololol
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
