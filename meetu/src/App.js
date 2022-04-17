77cde387de6e99d22b3c88ce2e37cce8b93c96d3
govgovnahr <govindn3@illinois.edu>
2022-04-16 22:23:30 -0500

    first day done

--- meetu/src/App.js
+++ meetu/src/App.js
@@ -7,11 +7,28 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          audrey has impostor syndrome :(
+          <h1>meetU</h1>
         </p>
         <a>
-          Hi Lololol
+          the future of connection
         </a>
+        <body>
+          <p></p>
+          {/* <!-- Features Section --> */}
+          <div class="main" id="about">
+              <div class="main__container">
+                  <div class="main__img--container">
+                      <a href="https://www.linkedin.com/in/chirag-rastogi-0baa72178/" target="_blank">
+                      </a>
+                  </div>
+                  <div class = "main__content">
+                      <h1>Who am I?</h1>
+                      <p>I am your mom</p>
+                          
+                  </div>
+              </div>
+          </div>
+        </body>
       </header>
     </div>
   );
